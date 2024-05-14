c0fbd8851c91dc05dc642e6860f01cf972703748
Andrii Nikitin <anikitin@suse.de>
2024-05-14 12:59:30 +0200

    t: Add diagnostic to leap test

--- t/02-leap.sh
+++ t/02-leap.sh
@@ -6,7 +6,7 @@ mkdir -p /usr/share/zypp/local/service/openSUSE/repo/
 ln -sf /opt/project/opensuse-leap-repoindex.xml /usr/share/zypp/local/service/openSUSE/repo/repoindex.xml
 zypper addservice /usr/share/zypp/local/service/openSUSE/ openSUSE
 
-zypper -vvv ref -s
+zypper -vvv ref -s || tail -n 2000 /var/log/zypper.log
 
 grep -r baseurl /etc/zypp/repos.d
 grep -r baseurl /etc/zypp/repos.d | grep -q cdn.opensuse.org
